b0fea7adf99fee20b4db846d9880c4c08d41b3de
Adrián Fernández <adrian-fernandez-amador@users.noreply.github.com>
2018-04-13 19:02:22 +0200

    EJ 1

--- pra3/pract3b.asm
+++ pra3/pract3b.asm
@@ -1,6 +1,19 @@
-<nombre módulo> SEGMENT BYTE PUBLIC 'CODE'
-ASSUME CS: <nombre módulo>
-…
-…
-<nombre módulo> ENDS
+_pract3b SEGMENT BYTE PUBLIC 'CODE'
+ASSUME CS: _pract3b
+PUBLIC: _calcularAciertos, _calcularSemiaciertos
+; --------------------------------
+; COMIENZO DE PRIMERA RUTINA
+_calcularAciertos PROC FAR
+	; GUARDAMOS BP EN LA PILA
+	PUSH BP
+	MOV BP, SP
+	
+	
+
+	; SACAMOS BP DE LA PILA
+	POP BP
+	RET
+_calcularAciertos ENDP
+; FINAL PRIMERA RUTINA
+_pract3b ENDS
 END
